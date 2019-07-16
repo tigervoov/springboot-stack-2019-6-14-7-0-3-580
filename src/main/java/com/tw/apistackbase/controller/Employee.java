f5d1508e6f3588ef2d35e3b6ed823a0418f9a542
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-16 18:24:45 +0800

    fish test of get

---
+++ src/main/java/com/tw/apistackbase/controller/Employee.java
@@ -0,0 +1,47 @@
+package com.tw.apistackbase.controller;
+
+public class Employee {
+    private String name;
+    private int id;
+    private int age;
+    private String gender;
+
+    public Employee(String name, int id, int age, String gender) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -1,19 +1,23 @@
 package com.tw.apistackbase.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListResourceBundle;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * Created by jxzhong on 18/08/2017.
  */
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/employees")
 public class HelloResource {
+    private List<Employee> result=new ArrayList<>();
 
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
@@ -22,6 +26,37 @@ public class HelloResource {
 
         return ResponseEntity.ok("Hello:" + userName);
     }
+    @GetMapping()
+    public ResponseEntity getAllEmployees(){
+        Employee employee=new Employee("aaa",11,18,"a1");
+        result.add(employee);
+//        result.toString();
+        return ResponseEntity.ok().body(result);
+    }
+    @PostMapping()
+    public ResponseEntity create(@RequestBody Employee employee){
+        result.add(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity put(@PathVariable int id,@RequestBody Employee employee){
+        List<Employee> changeEmployee=result.stream().filter(item->item.getId()==id).collect(Collectors.toList());
+        changeEmployee.get(0).setName(employee.getName());
+        changeEmployee.get(0).setAge(employee.getAge());
+        changeEmployee.get(0).setGender(employee.getGender());
+        return ResponseEntity.ok().body(result);
+
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable int id){
+        for(Employee employee:result){
+            if(employee.getId()==id){
+                result.remove(employee);
+            }
+        }
+        return ResponseEntity.ok().body(result);
+    }
+
 
 
 }
